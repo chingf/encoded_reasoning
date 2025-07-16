f15eeea77c5d44ff4335dcda1aaf561ab54ddef4
Ching Fang <chingfang17@gmail.com>
2025-07-16 15:08:43 +0000

    Update config for both remotes

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-storage_dir = '/n/holylfs06/LABS/krajan_lab/Lab/cfang/encoded_reasoning'
+import os
+
+is_kempner_cluster = os.path.exists('/n/holylfs06')
+if not is_kempner_cluster:
+    storage_dir = '/workspace/data/storage'
+    hf_cache_dir = '/workspace/data/huggingface-cache/hub'
+    os.environ['HF_HUB_CACHE'] = hf_cache_dir
+else:
+    storage_dir = '/n/holylfs06/LABS/krajan_lab/Lab/cfang/encoded_reasoning'
+    hf_cache_dir = '/n/holylfs06/LABS/krajan_lab/Lab/cfang/hf_cache'
