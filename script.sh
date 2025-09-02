a1ace719d4df633501aad71ea6127d3f83d198bf
Ching Fang <chingfang17@gmail.com>
2025-09-02 17:34:15 +0000

    Stashing changes

--- script.sh
+++ script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-for idx in {0..120000..5000}; do
+# Define batch size variable
+batch_size=750
+
+# Calculate max_samples (step size) as batch_size * 10
+max_samples=$((batch_size * 10))
+
+for idx in $(seq 54000 $max_samples 120000); do
     echo "Running with start_idx: $idx"
-    python 02_generate_transcript_llama3-70b_deepseek_distill.py --start_idx $idx
+    python 02_generate_transcript_llama3-70b_deepseek_distill.py --start_idx $idx --batch_size $batch_size --max_samples $max_samples
     
     # Check if the previous command was successful
     if [ $? -eq 0 ]; then
